841122937856f02975faad11c6ffb92a075f6be5
Sai <dgsai@vip.qq.com>
2020-04-09 03:15:02 +0800

    修改编译脚本。

--- project-build-and-docker-push.sh
+++ project-build-and-docker-push.sh
@@ -9,4 +9,9 @@ mvn dockerfile:build && \
 mvn dockerfile:tag@tag-version && \
 mvn dockerfile:push@push-latest && \
 mvn dockerfile:push@push-version && \
-say nice
+say nice && \
+docker stop css-spring && \
+# 加了 --rm 参数，当容器 stop 或 docker重启时，容器就会被删除。
+docker run -d -p 7788:8080 --name css-spring --rm registry.cn-shenzhen.aliyuncs.com/dgut_lab/css-spring-start-site && \
+\
+docker image prune -f
