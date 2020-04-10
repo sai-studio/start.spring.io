155ca87cd34cdf6b3ec1b668fb31d9dccf718d29
Sai <dgsai@vip.qq.com>
2020-04-11 04:04:20 +0800

    修改项目镜像打包脚本

--- project-build-and-docker-push.sh
+++ project-build-and-docker-push.sh
@@ -6,10 +6,12 @@ echo "清除缓存完成。"
 cd .. && \
 mvn clean source:jar package -DskipTests && \
 mvn dockerfile:build && \
-mvn dockerfile:tag@tag-version && \
+#mvn dockerfile:tag@tag-version && \
 mvn dockerfile:push@push-latest && \
-mvn dockerfile:push@push-version && \
+#mvn dockerfile:push@push-version && \
 say nice && \
+\
+docker image prune -f && \
 docker stop css-spring && \
 # 加了 --rm 参数，当容器 stop 或 docker重启时，容器就会被删除。
 docker run -d -p 7788:8080 --name css-spring --rm registry.cn-shenzhen.aliyuncs.com/dgut_lab/css-spring-start-site && \
